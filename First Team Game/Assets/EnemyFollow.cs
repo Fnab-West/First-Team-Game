c503899d5587190315b52a8a128101b423cc1413
Fnab-West <masonyeets234@gmail.com>
2021-04-13 19:18:20 -0400

    Almost Done

--- First Team Game/Assets/EnemyFollow.cs
+++ First Team Game/Assets/EnemyFollow.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+    public Transform target;
+    public Rigidbody2D rb;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("TrianglePlayer").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.up = (Vector2)target.position - (Vector2)transform.position;
+        rb.velocity = speed * transform.up * Time.deltaTime;
     }
 }
---
+++ First Team Game/Assets/spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform player;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < 3)
+        {
+            timer += Time.deltaTime;
+        }else if(timer >= 3)
+        {
+            timer = 0; 
+            Instantiate(enemy, new Vector3(Random.Range(-7.5f, 7.5f), Random.Range(4, -4)) + player.position, Quaternion.Euler(0, 0, 0), null);
+        }
+
+    }
+    
+}
